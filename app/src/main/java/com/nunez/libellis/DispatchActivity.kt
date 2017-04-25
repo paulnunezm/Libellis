d71d76e535d7afcf95a82e388c2169becd9338c6
Paul Nunez <paulnunezm@gmail.com>
2017-04-25 01:13:39 -0400

    added: dispatch activity.
    This decides wich activity to show when the user enters the application.

--- app/src/main/java/com/nunez/libellis/DispatchActivity.kt
+++ app/src/main/java/com/nunez/libellis/DispatchActivity.kt
@@ -1,12 +1,27 @@
 package com.nunez.libellis
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 class DispatchActivity : AppCompatActivity() {
 
+    lateinit var nextActivityIntent: Intent
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dispatch)
+
+        if(userLoggedIn()){
+            // go to updates activity
+        }else{
+            // go to login activity
+        }
+
+        startActivity(nextActivityIntent)
     }
+
+    fun userLoggedIn() = this.getSharedPreferences("user_prefs", 0)
+            .getBoolean("logged_in", false)
+
 }

1e86ba707a88c9fd2340672c9816807d45929d70
Paul Nunez <paulnunezm@gmail.com>
2017-04-25 01:18:40 -0400

    added: blank login and updates activities.

---
+++ app/src/main/java/com/nunez/libellis/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.nunez.libellis.login
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.nunez.libellis.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+    }
+}
---
+++ app/src/main/java/com/nunez/libellis/updates/UpdatesActivity.kt
@@ -0,0 +1,13 @@
+package com.nunez.libellis.updates
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.nunez.libellis.R
+
+class UpdatesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.updates_activity)
+    }
+}

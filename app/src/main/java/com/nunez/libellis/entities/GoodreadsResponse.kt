67223b9adf737135ea20f206a526c666d6d38bed
Paul Nunez <paulnunezm@gmail.com>
2017-04-29 18:12:00 -0400

    added: entities to handle the response objects.

--- app/src/main/java/com/nunez/libellis/entities/GoodreadsResponse.kt
+++ app/src/main/java/com/nunez/libellis/entities/GoodreadsResponse.kt
@@ -1,13 +1,16 @@
 package com.nunez.libellis.entities
 
+import org.simpleframework.xml.ElementList
 import org.simpleframework.xml.Root
 
 /**
  * Created by paulnunez on 4/25/17.
  */
-@Root(name = "GoodreadsResponse", strict = false)
-class GoodreadsResponse {
 
-//    @Element
-//    var user: User? = null
-}
+@Root(strict = false)
+class GoodreadsResponse(
+        @field:ElementList(name = "updates") var updates: ArrayList<Update>? = null
+
+
+        /** For better understanding about why the @field go to https://kotlinlang.org/docs/reference/annotations.html#annotation-use-site-targets */
+)

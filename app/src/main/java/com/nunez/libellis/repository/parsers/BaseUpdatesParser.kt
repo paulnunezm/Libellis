164251cda70a5e5f8e5f84f971418e868203d284
Paul Nunez <paulnunezm@gmail.com>
2017-04-29 19:00:51 -0400

    added: function with lambdas to perform the iteration in nodesChilds.
    To make the code more readable and avoid code duplication.

--- app/src/main/java/com/nunez/libellis/repository/parsers/BaseUpdatesParser.kt
+++ app/src/main/java/com/nunez/libellis/repository/parsers/BaseUpdatesParser.kt
@@ -9,23 +9,32 @@ import org.w3c.dom.NodeList
  */
 open class BaseUpdatesParser {
 
-    fun getNodeValue(node: Node): String{
+    fun getNodeValue(node: Node): String {
         return node.childNodes.item(0).nodeValue
+    }
+
+
+    /**
+     * Iterates ove a childList and return a node. This is for not repeat
+     * this lines everytime we're trying to parse a response.
+     */
+    fun iterateInChildNotes(nodeList: NodeList, f: (node: Node) -> Unit) {
+        for (i in 0..nodeList.length - 1) {
+            f(nodeList.item(i))
+        }
     }
 
     fun parseActor(actorNode: NodeList): User {
         val user = User()
 
-        for(i in 0..actorNode.length-1){
-            val actorChildrenNode = actorNode.item(i)
-
-            when(actorChildrenNode.nodeName){
-                "id" -> user.id = getNodeValue(actorChildrenNode)
-                "name" -> user.name = getNodeValue(actorChildrenNode)
-                "image_url" -> user.imageUrl = getNodeValue(actorChildrenNode)
-                "link" -> user.profileUrl = getNodeValue(actorChildrenNode)
+        iterateInChildNotes(actorNode, { actorNodeChild ->
+            when (actorNodeChild.nodeName) {
+                "id" -> user.id = getNodeValue(actorNodeChild)
+                "name" -> user.name = getNodeValue(actorNodeChild)
+                "image_url" -> user.imageUrl = getNodeValue(actorNodeChild)
+                "link" -> user.profileUrl = getNodeValue(actorNodeChild)
             }
-        }
+        })
 
         return user
     }

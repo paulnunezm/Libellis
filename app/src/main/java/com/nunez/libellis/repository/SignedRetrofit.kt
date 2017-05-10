7102880c48f08470dde6d695cffd1c32448b5d5f
Paul Nunez <paulnunezm@gmail.com>
2017-05-10 19:58:33 -0400

    edit: divided signedRetrofit into two.
    Created a signed okhttp class. To be able to perform calls in which the responses can't be coverted
    by a retrofitConverted class.

--- app/src/main/java/com/nunez/libellis/repository/SignedRetrofit.kt
+++ app/src/main/java/com/nunez/libellis/repository/SignedRetrofit.kt
@@ -1,15 +1,8 @@
 package com.nunez.libellis.repository
 
 import android.content.Context
-import android.util.Log
-import com.facebook.stetho.okhttp3.StethoInterceptor
-import com.nunez.libellis.BuildConfig
-import com.nunez.libellis.R
-import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.simplexml.SimpleXmlConverterFactory
-import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer
-import se.akerfeldt.okhttp.signpost.SigningInterceptor
 
 /**
  * Handles the retrofit signing process.
@@ -20,30 +13,10 @@ class SignedRetrofit(val context: Context) {
     val instance: Retrofit
         get() {
 
-            val prefs = context.getSharedPreferences(context.getString(R.string.user_prefs), 0)
-            val userKey = prefs.getString(context.getString(R.string.user_prefs_user_key), "")
-            val userSecret = prefs.getString(context.getString(R.string.user_prefs_user_secret), "")
-
-            Log.d("SignedRetrofit", "key=$userKey, secret $userSecret")
-
-            if(userKey.isEmpty()) throw IllegalArgumentException("user key is not present")
-            if(userSecret.isEmpty()) throw IllegalArgumentException("user secret is not present")
-
-            val consumer = OkHttpOAuthConsumer(BuildConfig.GOODREADS_API_KEY,
-                    BuildConfig.GOODREADS_API_SECRET)
-
-            consumer.setTokenWithSecret(userKey, userSecret)// this are the values that needs to be saved
-
-            val stethoInterceptor = StethoInterceptor()
-            val client = OkHttpClient.Builder()
-                    .addInterceptor(SigningInterceptor(consumer))
-                    .addNetworkInterceptor(stethoInterceptor)
-                    .build()
-
             val retrofit = Retrofit.Builder()
                     .baseUrl(GoodreadsService.BASE_URL)
                     .addConverterFactory(SimpleXmlConverterFactory.create())
-                    .client(client)
+                    .client(SignedHttpClient(context).instance)
                     .build()
 
             return retrofit

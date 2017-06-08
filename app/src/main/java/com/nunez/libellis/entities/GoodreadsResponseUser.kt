c0841b4aa155f63335602d2e45f62846cb7870d7
Paul Nunez <paulnunezm@gmail.com>
2017-06-08 19:06:11 -0400

    removed: user response object as it wont be needed anymore.
    Instead of using retrofit for this call now were using Okhttp and
    a custom xml parser.

--- app/src/main/java/com/nunez/libellis/entities/GoodreadsResponseUser.kt
+++
@@ -1,7 +0,0 @@
-package com.nunez.libellis.entities
-
-import org.simpleframework.xml.Attribute
-import org.simpleframework.xml.Root
-
-@Root(name = "user", strict = false)
-class GoodreadsUserResponse(@field:Attribute(name = "id") var id: String)

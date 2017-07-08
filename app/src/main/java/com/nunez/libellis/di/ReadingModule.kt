9eac97a36738734b62c71e1f9ed9b285fb1e24c3
Paul Nunez <paulnunezm@gmail.com>
2017-07-07 23:11:24 -0400

    edit: missing module annotation.

--- app/src/main/java/com/nunez/libellis/di/ReadingModule.kt
+++ app/src/main/java/com/nunez/libellis/di/ReadingModule.kt
@@ -5,10 +5,11 @@ import com.nunez.libellis.main.reading.ReadingContract
 import com.nunez.libellis.main.reading.ReadingInteractor
 import com.nunez.libellis.main.reading.ReadingPrensenter
 import com.nunez.libellis.repository.SignedRetrofit
+import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
 
-
+@Module
 class ReadingModule(val view: ReadingContract.View) {
     lateinit var interactor: ReadingInteractor
 

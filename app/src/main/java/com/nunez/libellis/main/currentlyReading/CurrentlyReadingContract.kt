3a9ce567b0549a83472121b9f3fecd8811850409
Paul Nunez <paulnunezm@gmail.com>
2017-07-07 22:46:47 -0400

    added: Currently reading contract.

---
+++ app/src/main/java/com/nunez/libellis/main/currentlyReading/CurrentlyReadingContract.kt
@@ -0,0 +1,26 @@
+package com.nunez.libellis.main.currentlyReading
+
+import com.nunez.libellis.entities.Review
+import com.nunez.libellis.entities.Update
+import io.reactivex.Observable
+
+interface CurrentlyReadingContract {
+
+    interface View {
+        fun showBooks(books: List<Review>)
+        fun showLoading()
+        fun showNoBooks()
+        fun showMessage(message: String, error: Boolean = false)
+    }
+
+    interface Presenter {
+        fun getBooks()
+        fun sendUpdates(updates: List<Update>)
+        fun showError(message: String)
+    }
+
+    interface Interactor {
+        fun requestBooks(): Observable<List<Review>>
+    }
+
+}

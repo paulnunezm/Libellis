1e937d456199c143852adfd379fb2e8c6ee43f3e
Paul Nunez <paulnunezm@gmail.com>
2017-07-10 21:11:12 -0400

    added: Reading fragment implementation.

--- app/src/main/java/com/nunez/libellis/main/reading/ReadingFragment.kt
+++ app/src/main/java/com/nunez/libellis/main/reading/ReadingFragment.kt
@@ -2,14 +2,25 @@ package com.nunez.libellis.main.reading
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.nunez.libellis.LibellisApp
 import com.nunez.libellis.R
+import com.nunez.libellis.di.AppModule
+import com.nunez.libellis.di.DaggerReadingComponent
+import com.nunez.libellis.di.ReadingModule
 import com.nunez.libellis.entities.Review
+import com.nunez.libellis.showSnackbar
+import kotlinx.android.synthetic.main.fragment_currently_reading.*
+import javax.inject.Inject
 
 
 class ReadingFragment : Fragment(), ReadingContract.View {
+
+    @Inject lateinit var interactor: ReadingInteractor
+    lateinit var presenter: ReadingPrensenter
 
     companion object {
         val TAG = "CurrentlyReading"
@@ -26,24 +37,35 @@ class ReadingFragment : Fragment(), ReadingContract.View {
         // Inflate the layout for this fragment
         val view = inflater!!.inflate(R.layout.fragment_currently_reading, container, false)
 
+        DaggerReadingComponent.builder()
+                .appModule(AppModule(this.activity.application as LibellisApp))
+                .readingModule(ReadingModule(this))
+                .build()
+                .inject(this)
+
+        presenter = ReadingPrensenter(this, interactor)
+        presenter.getBooks()
 
         return view
     }
 
     override fun showBooks(books: List<Review>) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        readingRecycler.layoutManager = LinearLayoutManager(activity)
+        readingRecycler.setHasFixedSize(true)
+        readingRecycler.adapter = ReadingAdapter(books, {
+            id ->
+            readingRecycler.showSnackbar("Id $id")
+        })
     }
 
     override fun showLoading() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun showNoBooks() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun showMessage(message: String, error: Boolean) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        readingRecycler.showSnackbar(message)
     }
 
 }

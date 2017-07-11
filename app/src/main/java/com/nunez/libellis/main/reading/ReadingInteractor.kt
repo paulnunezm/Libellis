eefc246cef339374cd05d90d290fbbc43bcf2fde
Paul Nunez <paulnunezm@gmail.com>
2017-07-10 21:11:39 -0400

    added: reading interactor implementation.

--- app/src/main/java/com/nunez/libellis/main/reading/ReadingInteractor.kt
+++ app/src/main/java/com/nunez/libellis/main/reading/ReadingInteractor.kt
@@ -1,9 +1,17 @@
 package com.nunez.libellis.main.reading
 
 import android.content.Context
+import com.nunez.libellis.BuildConfig
+import com.nunez.libellis.R
+import com.nunez.libellis.enqueue
+import com.nunez.libellis.entities.GoodreadsResponse
 import com.nunez.libellis.entities.Review
+import com.nunez.libellis.repository.GoodreadsService
 import com.nunez.libellis.repository.SignedRetrofit
 import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import retrofit2.Call
 import javax.inject.Inject
 
 class ReadingInteractor
@@ -12,51 +20,42 @@ class ReadingInteractor
         val signedRetrofit: SignedRetrofit
 ) : ReadingContract.Interactor {
 
+    companion object {
+        const val TAG = "ReadingInteractor"
+    }
+
     override fun requestBooks(): Observable<List<Review>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        /*
-        Log.d(TAG, "creating observable")
-        Observable.create<Unit>({
-            val retrofit = Retrofit.Builder()
-                    .baseUrl(GoodreadsService.BASE_URL)
-                    .addConverterFactory(SimpleXmlConverterFactory.create())
-                    .build()
+        val retrofit = signedRetrofit.instance
+
+        return Observable.create<List<Review>>({
+            subscriber ->
 
             val goodreads = retrofit.create(GoodreadsService::class.java)
+
             val userId = context.getSharedPreferences(context.getString(R.string.user_prefs), 0)
                     .getString(context.getString(R.string.user_prefs_user_id), "")
 
-            val apiKey = BuildConfig.GOODREADS_API_KEY
+            val response: Call<GoodreadsResponse> = goodreads.getBooksOnShelf(
+                    userId = userId,
+                    shelfName = "currently-reading",
+                    search = "",
+                    developerKey = BuildConfig.GOODREADS_API_KEY)
 
-            try {
-                val response: Call<GoodreadsResponse> = goodreads.getBooksOnShelf(
-                        userId = userId,
-                        shelfName = "currently-reading",
-                        search = "",
-                        developerKey = apiKey
-                )
+            response.enqueue({
+                response ->
+                if (response.isSuccessful) {
+                    val r = response.body()?.reviews as List<Review>
+                    subscriber.onNext(r)
+                    subscriber.onComplete()
+                } else {
+                    subscriber.onError(Throwable("Response unsuccessful"))
+                }
+            }, {
+                Throwable(it)
+                subscriber.onError(it)
+            })
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
 
-                response.enqueue(
-                        {
-                            response: retrofit2.Response<GoodreadsResponse> ->
-                            if (response.isSuccessful) {
-                                val r: String = response.body()?.reviews?.get(0)?.book.toString()
-                                Log.d(TAG,"sucessfull $r" )
-                            } else {
-                                Log.d(TAG, "not successfull ${response.body().toString()}")
-                            }
-                        },
-                        {
-                            t ->
-                            Log.e(TAG, t.message, t)
-                        })
-            } catch (e: Throwable) {
-                Log.e(TAG, e.message, e)
-            }
-        })
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread()).subscribe({},{})
-
-         */
     }
 }

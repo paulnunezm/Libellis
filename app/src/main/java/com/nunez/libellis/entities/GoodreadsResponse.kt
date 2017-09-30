192ed507d02284abd6b719514924690e43e59b01
Paul Nunez <paulnunezm@gmail.com>
2017-09-29 22:46:19 -0400

    added: a field to parse the review userstatus response.

--- app/src/main/java/com/nunez/libellis/entities/GoodreadsResponse.kt
+++ app/src/main/java/com/nunez/libellis/entities/GoodreadsResponse.kt
@@ -1,5 +1,6 @@
 package com.nunez.libellis.entities
 
+import org.simpleframework.xml.Element
 import org.simpleframework.xml.ElementList
 import org.simpleframework.xml.Root
 
@@ -9,7 +10,8 @@ class GoodreadsResponse(
          * for each field.*/
         @field:ElementList(name = "updates", required = false) var updates: ArrayList<Update>? = null,
         @field:ElementList(name = "shelves", required = false) var shelves: List<Shelve>? = null,
-        @field:ElementList(name = "reviews", required = false) var reviews: List<Review>? = null
+        @field:ElementList(name = "reviews", required = false) var reviews: List<Review>? = null,
+        @field:Element(name = "review", required = false) var review: Review? = null
 
         /** For better understanding about why the @field go to https://kotlinlang.org/docs/reference/annotations.html#annotation-use-site-targets */
 )

893c529216ca0c18bd730521e232dccd60b40620
Paul Nunez <paulnunezm@gmail.com>
2017-10-14 21:01:38 -0400

    added: methods for updating the book reading location and for when the user has finished the book.

--- app/src/main/java/com/nunez/libellis/main/reading/updateProgress/UpdateProgressSheetInteractor.kt
+++ app/src/main/java/com/nunez/libellis/main/reading/updateProgress/UpdateProgressSheetInteractor.kt
@@ -1,61 +1,83 @@
 package com.nunez.libellis.main.reading.updateProgress
 
-import android.util.Log
 import com.nunez.libellis.entities.GoodreadsResponse
 import com.nunez.libellis.entities.Review
+import com.nunez.libellis.entities.UpdateProgress
 import com.nunez.libellis.repository.GoodreadsService
+import com.nunez.libellis.views.UpdateProgressView
 import io.reactivex.Completable
 import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 
-class UpdateProgressSheetInteractor(private val goodreadsService: GoodreadsService) : UpdateProgressSheetContract.Interactor {
+class UpdateProgressSheetInteractor(
+        private val goodreadsService: GoodreadsService
+) : UpdateProgressSheetContract.Interactor {
 
     override fun getBookReadingInfo(reviewId: String): Observable<Review> {
         // TODO: check internet connection
 
-        return Observable.create<Review>({
-            subscriber ->
-            Log.d("progress", " looking progress")
+        return Observable.create<Review>({ subscriber ->
             try {
                 val response = goodreadsService.getReview(com.nunez.libellis.BuildConfig.GOODREADS_API_KEY, reviewId)
-
                 response.enqueue(object : Callback<GoodreadsResponse> {
-
                     override fun onResponse(call: Call<GoodreadsResponse>?, response: Response<GoodreadsResponse>?) {
                         val review = response?.body()?.review
-
-                        Log.i("progress", "onResponse ${review?.book?.title}")
-
                         if (review != null) {
-                            Log.i("progress", "onNext")
                             subscriber.onNext(review)
                             subscriber.onComplete()
-                        }else{
-                            Log.e("progress", "Response error")
+                        } else {
                             subscriber.onError(Throwable("No review founded"))
                         }
                     }
-
                     override fun onFailure(call: Call<GoodreadsResponse>?, t: Throwable?) {
-                        Log.d("progress", "on failure -> ${t?.message}")
+                        t?.printStackTrace()
                         subscriber.onError(Throwable("No review founded"))
-
                     }
                 })
-
             } catch (e: Throwable) {
-                Log.e("progress", e.message)
-
+                e.printStackTrace()
                 subscriber.onError(e)
             }
         })
     }
 
-    override fun updateBookReadingLocation(): Completable {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun updateBookReadingLocation(update: UpdateProgress): Completable {
+        if (update.type == UpdateProgressView.TYPE_PERCENT) {
+            return updateBookWithPercent(update)
+        } else {
+           return updateBookWithPages(update)
+        }
     }
 
+    override fun bookFinished(update: UpdateProgress): Completable {
+        return goodreadsService.sendBookFinished(
+                update.id,
+                update.comment,
+                update.rating)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+    }
+
+    private fun updateBookWithPages(update: UpdateProgress): Completable {
+        return goodreadsService.sendBookUpdate(
+                id = update.id,
+                page = update.value,
+                comment = update.comment)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+    }
+
+    private fun updateBookWithPercent(update: UpdateProgress): Completable {
+        return goodreadsService.sendBookUpdate(
+                id = update.id,
+                percent = update.value,
+                comment = update.comment)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+    }
 }

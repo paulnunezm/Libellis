f2f84e8b727a304aec7afc8b4dcdcbce1b84510f
Paul Nunez <paulnunezm@gmail.com>
2017-10-27 23:29:21 -0400

    removed: unnecessary layout for the dispatch activity.

--- app/src/main/java/com/nunez/libellis/DispatchActivity.kt
+++ app/src/main/java/com/nunez/libellis/DispatchActivity.kt
@@ -10,7 +10,6 @@ class DispatchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dispatch)
 
         val nextActivityIntent =
                 if (userLoggedIn())

d9782bcfa115825db8342349d948548794b3b6ee
Paul Nunez <paulnunezm@gmail.com>
2017-11-19 18:00:42 -0400

    edit: changed ReadingInteractor dependencies and commented the implementation.
    Dependencies where change to depend on interfaces instead of concrete implementations. The main meaning of this is to be able of TDD while reacreating this class.

--- app/src/main/java/com/nunez/libellis/main/reading/ReadingInteractor.kt
+++ app/src/main/java/com/nunez/libellis/main/reading/ReadingInteractor.kt
@@ -1,20 +1,18 @@
 package com.nunez.libellis.main.reading
 
-import android.content.Context
-import com.nunez.libellis.NoConnectivityException
+import com.nunez.libellis.ConnectivityChecker
+import com.nunez.libellis.entities.CurrentlyReadingBook
 import com.nunez.libellis.entities.raw.Review
-import com.nunez.libellis.getUserId
-import com.nunez.libellis.isConnectedToTheInternet
 import com.nunez.libellis.repository.GoodreadsService
-import com.nunez.libellis.repository.SignedRetrofit
+import com.nunez.libellis.repository.ReactiveStore
 import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 
 class ReadingInteractor
 constructor(
-        val context: Context,
-        private val signedRetrofit: SignedRetrofit
+//        val context: Context,
+        private val repository: ReactiveStore<String, CurrentlyReadingBook>,
+        private val connectivityChecker: ConnectivityChecker,
+        private val goodreadsService: GoodreadsService
 ) : ReadingContract.Interactor {
 
     companion object {
@@ -23,29 +21,30 @@ constructor(
 
     override fun requestBooks(): Observable<List<Review>> {
         return Observable.defer {
-           try{
-               if (context.isConnectedToTheInternet()) {
+//           try{
+//               if (context.isConnectedToTheInternet()) {
                    requestBooksFromApi()
-               } else {
-                  throw NoConnectivityException()
-               }
-           }catch (e: NoConnectivityException){
-                Observable.error<List<Review>> { e }
-           }
+//               } else {
+//                  throw NoConnectivityException()
+//               }
+//           }catch (e: NoConnectivityException){
+//                Observable.error<List<Review>> { e }
+//           }
         }
     }
 
     private fun requestBooksFromApi(): Observable<List<Review>> {
-        val retrofit = signedRetrofit.instance
-        val goodreads = retrofit.create(GoodreadsService::class.java)
-        return goodreads.getBooksOnShelfRX(
-                userId = context.getUserId(),
-                shelfName = "currently-reading")
-                .map {
-                    it.reviews ?: emptyList()
-                }
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
+//        val retrofit = signedRetrofit.instance
+//        val goodreads = retrofit.create(GoodreadsService::class.java)
+//        return goodreads.getBooksOnShelfRX(
+//                userId = context.getUserId(),
+//                shelfName = "currently-reading")
+//                .map {
+//                    it.reviews ?: emptyList()
+//                }
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+        TODO()
     }
 
 }

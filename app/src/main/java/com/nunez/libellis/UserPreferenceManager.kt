320507e19507f8dec047a97cc7bee007c2c99f94
Paul Nunez <paulnunezm@gmail.com>
2017-11-22 20:17:41 -0400

    added: a variable to get the userId and a function to set the currentlyReading books are cached.

--- app/src/main/java/com/nunez/libellis/UserPreferenceManager.kt
+++ app/src/main/java/com/nunez/libellis/UserPreferenceManager.kt
@@ -6,12 +6,15 @@ import android.content.SharedPreferences
 interface UserPreferenceManager {
     fun isUserLoggedIn(): Boolean
     fun isCurrentlyReadingBooksSavedInCache(): Boolean
+    fun setReadingBooksSavedInCache()
 }
 
 class UserPrefsManager(private val context: Context) : UserPreferenceManager {
 
     private val preference: SharedPreferences =
-            context.getSharedPreferences(getStringResource(R.string.user_prefs), 0)
+            context.getSharedPreferences(context.getString(R.string.user_prefs), 0)
+
+    val userId = preference.getString(getStringResource(R.string.user_prefs_user_id), "")
 
     override fun isUserLoggedIn(): Boolean = getBooleanPreference(R.string.user_prefs_logged_in)
 
@@ -25,4 +28,11 @@ class UserPrefsManager(private val context: Context) : UserPreferenceManager {
                                      defaultValue: Boolean = false): Boolean {
         return preference.getBoolean(getStringResource(preferenceStringResource), defaultValue)
     }
+
+    override fun setReadingBooksSavedInCache() {
+        val editor = preference.edit()
+        editor.putBoolean(getStringResource(R.string.user_prefs_currently_reading_cached), true)
+        editor.apply()
+    }
+
 }
